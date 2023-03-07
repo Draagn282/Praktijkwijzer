419b575c6188b61b6bcb5a5747ac36c828450b70
Draagn282 <94443911+Draagn282@users.noreply.github.com>
2023-03-07 09:48:52 +0100

    de eerste commit

---
+++ pages/administrator.php
@@ -0,0 +1,19 @@
+<!--klachten welkomen en gegevens regristeren
+    afhandelen betalingen
+    bijhouden gegevens bezetingen
+    telefonische reserveringen verwerken met mappen/database
+    klachten formulier 
+
+    rooster van personeel beheren
+    verwelkomen gasten 
+
+
+
+    syteem: plaats bediening
+    - hoeveel plaatsen
+    - welke plaatsen
+    - inschijven klanten
+
+    systeem: klachten
+    - ''''
+-->
---
+++ pages/cleaner.php
@@ -0,0 +1,21 @@
+<!-- schoonmaken
+
+
+    syteem: klachten
+    - klacht
+    - datum
+    - datum eersts
+    - gewicht -> administrator kiest
+    - klachten na week prioriteit
+    - klacht aangewezen voor medewerker
+
+    systeem: bezetting
+    - naam klant
+    - plaats
+    - tijd aankomen
+    - tijd vertrek
+    
+    systeem: weekoverzicht
+    - plaats tijd bezet
+    - plaats medewerker roosteren
+-->
