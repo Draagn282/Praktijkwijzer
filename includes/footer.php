426523acfbff1e77c0ace8e8c28489980bc6525a
merijnkoppes <93912577+merijnkoppes@users.noreply.github.com>
2023-03-28 09:44:37 +0200

    homepagina

---
+++ includes/footer.php
@@ -0,0 +1,4 @@
+
+    <footer>
+		<p>&copy; 2023 Camping Ground. All rights reserved.</p>
+	</footer>
--- includes/header.php
+++ includes/header.php
@@ -4,14 +4,31 @@
             <button type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler navbar-toggler-right"><i class="fa fa-bars"></i></button>
             
             <div id="navbarSupportedContent" class="collapse navbar-collapse">
-                <ul class="navbar-nav ml-auto">
+            <ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Campsites</a></li>
+				<li><a href="#">Activities</a></li>
+				<li><a href="#">Contact Us</a></li>
+			</ul>
+            </div>
+        </div>
+    </nav>
+</header>
+<!--
+<h1>Camping Ground</h1>
+		<nav>
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Campsites</a></li>
+				<li><a href="#">Activities</a></li>
+				<li><a href="#">Contact Us</a></li>
+			</ul>
+		</nav> 
+                        <ul class="navbar-nav ml-auto">
                     <li class="nav-item active"><a href="#" class="nav-link text-uppercase font-weight-bold">Home <span class="sr-only">(current)</span></a></li>
                     <li class="nav-item"><a href="#" class="nav-link text-uppercase font-weight-bold">About</a></li>
                     <li class="nav-item"><a href="#" class="nav-link text-uppercase font-weight-bold">Gallery</a></li>
                     <li class="nav-item"><a href="#" class="nav-link text-uppercase font-weight-bold">Portfolio</a></li>
                     <li class="nav-item"><a href="#" class="nav-link text-uppercase font-weight-bold">Contact</a></li>
                 </ul>
-            </div>
-        </div>
-    </nav>
-</header>
+-->
