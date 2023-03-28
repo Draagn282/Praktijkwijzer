627e46392e96b5fbcd607f47887b90af29c3fde3
merijnkoppes <93912577+merijnkoppes@users.noreply.github.com>
2023-03-28 09:34:27 +0200

    homepagina

--- index.php
+++ index.php
@@ -5,8 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/nav-foot.css">
 </head>
 <body>
+<?php include_once("includes/header.php"); ?>
     <h1>LMAO</h1>
 </body>
 </html>
