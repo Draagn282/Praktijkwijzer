76bc9bfc6eb0a40964e7b251dcd93bf40e60772a
Draagn282 <94443911+Draagn282@users.noreply.github.com>
2023-05-23 14:02:58 +0200

    noghitn yet

--- pages/cleaner.php
+++ pages/cleaner.php
@@ -19,3 +19,167 @@
     - plaats tijd bezet
     - plaats medewerker roosteren
 -->
+
+<?php include "../includes/header.php" ?>
+<link rel="stylesheet" href="../css/styles.css">
+
+
+<h1>Cleaner pagina</h1>
+<div class="Admin_Page">
+    <div class="Admin_Subject">
+    <div class="Admin_Section">
+        <div class="Section_title">
+            <h3>Klachten</h3>
+        </div>
+        <div class="Section_QuickAcces">
+            <p>...Total Complaints...</p>
+            <p>...</p>
+        </div>
+        <div class="Section_scroll">
+            <div class="complaint">
+                <div class="complaint_data">
+                    <div class="complaint_user">
+                        <p>...User...</p>
+                    </div>      
+                    <div class="complaint_date">
+                        <p>...Date...</p>
+                    </div>
+                    <div class="complaint_date">
+                        <p>...Complete...</p>
+                    </div>
+                    <div class="complaint_weight">
+                        <p>...Weight...</p>
+                    </div>
+                    
+                </div>
+                <div class="complaint_text">
+                    <p>...This is a lot of text, but i do non know what to put here, so i am going to type some stuff to see if it fits...</p>
+                </div>
+
+                <div class="complaint_buttons">
+                    <button style="background-color: Green;">...complete...</button>
+                    <button style="background-color: cyan;">...edit...</button>
+                    <button style="background-color: red;">...delete...</button>
+                </div>
+            </div>
+            <div class="complaint">
+                <div class="complaint_data">
+                    <div class="complaint_user">
+                        <p>...User...</p>
+                    </div>      
+                    <div class="complaint_date">
+                        <p>...Date...</p>
+                    </div>
+                    <div class="complaint_date">
+                        <p>...Complete...</p>
+                    </div>
+                    <div class="complaint_weight">
+                        <p>...Weight...</p>
+                    </div>
+                </div>
+                <div class="complaint_text">
+                    <p>...This is even more text to see if i am a good blind typer, it looks like its working, but looking goed faster cuz i can see the things. THere  THere  THere  THere  THere  THere  THere  THere  THere  THere  THere  THere  THere  THere  THere  THere  THere  THere  THere  THere  THere  THere  THere a lot of text, but i do non know what to put here, so i am going to type some stuff to see if it fits...</p>
+                </div>
+
+                <div class="complaint_buttons">
+                    <button style="background-color: Green;">...complete...</button>
+                    <button style="background-color: cyan;">...edit...</button>
+                    <button style="background-color: red;">...delete...</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="Admin_Section">
+    <div class="Section_title">
+            <h3>Velden</h3>
+        </div>
+        <div class="Section_QuickAcces">
+            <p>...Totaal...</p>
+            <p>...Bezet...</p>
+            <p>...vrij...</p>
+            <p>...%over...</p>
+        </div>
+        <div class="Section_scroll">
+            <div class="field">
+                <div class="field_data">
+                        <p>...Field ID...</p>
+                        <p>...Field...</p>
+                        <p>...Current Status...</p>
+                        <p>...Date Taken...</p>
+                        <p>...Date Free...</p>
+                        <p>...Customer ID...</p>
+                        <p>...Customer Name...</p>
+                        <p>...Customer ID...</p>
+                </div>
+                <div class="complaint_buttons">
+                    <button>...edit...</button>
+                </div>
+            </div>
+            <div class="field">
+                <div class="field_data">
+                        <p>...Field ID...</p>
+                        <p>...Field...</p>
+                        <p>...Current Status...</p>
+                        <p>...Date Taken...</p>
+                        <p>...Date Free...</p>
+                        <p>...Customer ID...</p>
+                        <p>...Customer Name...</p>
+                        <p>...Customer ID...</p>
+                </div>
+                <div class="complaint_buttons">
+                    <button>...edit...</button>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <div class="Admin_Section">
+    <div class="Section_title">
+            <h3>Users</h3>
+        </div>
+        <div class="Section_QuickAcces">
+            <p>...Total Users...</p>
+            <p>...Amin...</p>
+            <p>...cleaner...</p>
+            <p>...Amin...</p>
+        </div>
+        <div class="Section_scroll">
+        <div class="Section_scroll">
+            <div class="User">
+                <div class="field_data">
+                        <p>...User ID...</p>
+                        <p>...User Role...</p>
+                        <p>...User Name...</p>
+                        <p>...User Mail...</p>
+                        <p>...User Address...</p>
+                        <p>...User Birthdate...</p>
+                </div>
+                <div class="complaint_buttons">
+                    <button>...edit...</button>
+                    <button>...delete...</button>
+                </div>
+            </div>
+            <div class="User">
+                <div class="field_data">
+                        <p>...User ID...</p>
+                        <p>...User Role...</p>
+                        <p>...User Name...</p>
+                        <p>...User Mail...</p>
+                        <p>...User Address...</p>
+                        <p>...User ...</p>
+                        <p>...Customer Name...</p>
+                </div>
+                <div class="complaint_buttons">
+                    <button>...edit...</button>
+                    <button>...delete...</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+    
+</div>
+
+
+<?php include '../includes/footer.php'?>
