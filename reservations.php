b45bbe1f504660b0e112db549377c350a5202719
amberopsteen <amber-opsteen@hotmail.com>
2023-06-05 16:44:43 +0200

    Reserveringspagina gemaakt. Je kan nu reserveren en je krijgt daarna een bevestingspagina te zien.

--- reservations.php
+++ reservations.php
@@ -3,7 +3,7 @@
 <body>
 <div class="formbold-main-wrapper">
   <div class="formbold-form-wrapper">
-    <form method="POST">
+    <form action="php/reserveren.php" method="POST">
         <h2>Reserveer hier</h2>
       <div class="flex flex-wrap formbold--mx-3">
         <div class="w-full sm:w-half formbold-px-3">
@@ -24,65 +24,48 @@
         <label for="guest" class="formbold-form-label">
           E-mailadres
         </label>
-        <input type="email" name="guest" id="guest" placeholder="e-mailadres" class="formbold-form-input" />
+        <input type="email" name="email" id="guest" placeholder="e-mailadres" class="formbold-form-input" />
       </div>
 
       <div class="formbold-mb-5">
         <label for="guest" class="formbold-form-label">
           Hoeveel gasten neem je mee?
         </label>
-        <input type="number" name="guest" id="guest" placeholder="2" class="formbold-form-input" />
+        <input type="number" name="guest" id="guest" placeholder="" class="formbold-form-input" />
       </div>
 
       <div class="formbold-mb-5">
         <label for="guest" class="formbold-form-label">
           Selecteer accomodatie
         </label>
-        <select class="formbold-form-input" id="accomodatie" name="accomodatie">
+        <select class="formbold-form-input" id="accomodatie" name="accomodation">
             <option value="caravan">Staanplaats caravan</option>
             <option value="camper">Staanplaats camper</option>
             <option value="tent">Staanplaats tent</option>
             <option value="Stacaravan">Stacaravan</option>
   </select>
       </div>
-      
       <div class="flex flex-wrap formbold--mx-3">
         <div class="w-full sm:w-half formbold-px-3">
-          <div class="formbold-mb-5 w-full">
-            <label for="date" class="formbold-form-label"> Aankomst datum </label>
-            <input type="date" name="date" id="date" class="formbold-form-input" />
+          <div class="formbold-mb-5">
+            <label for="date" class="formbold-form-label">Aankomst datum</label>
+            <input type="date" name="arrivalDate" id="arrivalDate" class="formbold-form-input" />
           </div>
         </div>
         <div class="w-full sm:w-half formbold-px-3">
           <div class="formbold-mb-5">
-            <label for="time" class="formbold-form-label"> Geschatte aankomst tijd </label>
-            <input type="time" name="time" id="time" class="formbold-form-input" />
-          </div>
-        </div>
-      </div>
-      <div class="flex flex-wrap formbold--mx-3">
-        <div class="w-full sm:w-half formbold-px-3">
-          <div class="formbold-mb-5 w-full">
-            <label for="date" class="formbold-form-label"> Vertrek datum </label>
-            <input type="date" name="date" id="date" class="formbold-form-input" />
-          </div>
-        </div>
-        <div class="w-full sm:w-half formbold-px-3">
-          <div class="formbold-mb-5">
-            <label for="time" class="formbold-form-label"> Geschatte vertrek tijd </label>
-            <input type="time" name="time" id="time" class="formbold-form-input" />
+            <label for="date" class="formbold-form-label">Vertrek datum</label>
+            <input type="date" name="departureDate" id="departureDate" class="formbold-form-input" />
           </div>
         </div>
       </div>
 
       <div>
-        <button class="formbold-btn">Verzenden</button>
+        <input type="submit" name="Verzenden" value="verzenden" class="formbold-btn">
       </div>
     </form>
   </div>
 </div>
-
     <?php include_once("./includes/footer.php"); ?>
-
 </body>
 </html>
