4a56ce2261edc6af792a5c0c8774b51f1dcac1a3
Draagn282 <94443911+Draagn282@users.noreply.github.com>
2023-06-06 14:20:45 +0200

    over ons, nog niet af maar amber wil dat ik push

--- aboutUs.php
+++ aboutUs.php
@@ -2,8 +2,39 @@
 ?>
 <body>
 <div class="about-us-container">
-    <h1>Over ons</h1>
-    <h3></h3>
+    <div class="formbold-main-wrapper">
+        <div class="formbold-form-wrapper">
+            <h1>Over ons</h1>
+            <h3> Geschreven door Daan iguess, ff kijken goe groot dit is</h3>
+            <p>
+                Wij bij deze plek hebben veel leifde voor onze klanten
+                wij zorgen ervoor dat iedereen gelijk eerlijk en cool is
+                als je een probleem hebt loop maar de berg op
+                daar zit geeen monster dat je op eet 
+                nee echt niet.
+                Nu stop ik de tekst van jotaro vs dio hier zodat er tekst gevuld owrd om te kijkern
+                hoe er er uitzierDIO: I've already taken care of Kakyoin, too.
+
+                DIO: Polnareff must be hiding somewhere... Hah. But it doesn't matter.
+
+                DIO: You're next, Jotaro!
+
+                JOTARO: You bastard. DIO!
+
+                DIO: Oh? You're approaching me? Instead of running away, you're coming right to me?
+
+                Even Though your grandfather, Joseph, told you the secret of The World, *dead Joseph pics*
+
+                like an exam student scrambling to finish the problems on an exam until the last moments
+
+                before the chime?
+
+                JOTARO: I can't beat the shit out of you without getting closer.
+
+                DIO: Oh ho! Then come as close as you like.
+            </p>
+        </div>
+    </div>
 </div>
     <?php include_once("./includes/footer.php"); ?>
 
